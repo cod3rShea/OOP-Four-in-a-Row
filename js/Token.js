5e04698f829dabfbec3aa999eff12e8221414db5
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2021-01-30 15:10:14 -0500

    forgot ;

--- js/Token.js
+++ js/Token.js
@@ -1,7 +1,7 @@
 class Token {
     constructor(playName, playID, playColor, active = false) {
         this.playName = playerName;
-        this.playerID = playerID
+        this.playerID = playerID;
         this.playerColor = playerColor;
         this.active = active;
         this.token = [];
