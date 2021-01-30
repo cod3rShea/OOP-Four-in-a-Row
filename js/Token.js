a33fcb7df76ea198aa0a3078203486f44fc93eff
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2021-01-30 15:09:14 -0500

    Start building out the player properties

--- js/Token.js
+++ js/Token.js
@@ -1,4 +1,10 @@
 class Token {
+    constructor(playName, playID, playColor, active = false) {
+        this.playName = playerName;
+        this.playerID = playerID
+        this.playerColor = playerColor;
+        this.active = active;
+        this.token = [];
+    }
 
-    
 }
